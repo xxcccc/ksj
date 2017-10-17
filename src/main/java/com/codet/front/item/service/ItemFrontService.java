edfef72040ccbaf0826e3c941abb8bb3f0800e2c
LL <974019777@qq,com>
2017-10-17 10:05:20 +0800

    改错

--- src/main/java/com/codet/front/item/service/ItemFrontService.java
+++ src/main/java/com/codet/front/item/service/ItemFrontService.java
@@ -1,5 +1,13 @@
 package com.codet.front.item.service;
 
+import java.util.List;
+
+import com.codet.pojo.Custom;
+import com.codet.pojo.Item;
+
 public interface ItemFrontService {
 
+	//通过商品名查找客户
+		public List<Item> findItemByItemName(String itemName) ;
+		
 }
--- src/main/java/com/codet/front/item/service/imlp/ItemFrontServiceImpl.java
+++ src/main/java/com/codet/front/item/service/imlp/ItemFrontServiceImpl.java
@@ -1,6 +1,18 @@
 package com.codet.front.item.service.imlp;
 
-public class ItemFrontServiceImpl  {
+import java.util.List;
 
+import com.codet.front.item.service.ItemFrontService;
+import com.codet.pojo.Item;
+
+public class ItemFrontServiceImpl implements ItemFrontService {
+
+	@Override
+	public List<Item> findItemByItemName(String itemName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 	
 }
